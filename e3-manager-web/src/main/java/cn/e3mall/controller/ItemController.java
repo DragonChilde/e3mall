4b94c8d29e73f6531b03da206766172aee266c33
dragon <fantasydragon_echo@163.com>
2018-10-19 17:40:51 +0800

    new add

---
+++ e3-manager-web/src/main/java/cn/e3mall/controller/ItemController.java
@@ -0,0 +1,21 @@
+package cn.e3mall.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ItemController {
+	
+	/*@Autowired
+	private ItemService itemService;
+
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getList(@PathVariable Long itemId)
+	{
+		return itemService.getItemById(itemId);
+	}*/
+}
